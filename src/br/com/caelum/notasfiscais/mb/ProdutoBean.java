e77f6571c5a4f0f4c335e42a8ac89caaa2ffdc97
jcmojj <jcmojj@gmail.com>
2016-12-08 20:26:02 -0200

    3a aula

--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,6 +1,5 @@
 package br.com.caelum.notasfiscais.mb;
 
-
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -12,7 +11,7 @@ import br.com.caelum.notasfiscais.modelo.Produto;
 public class ProdutoBean {
 	private Produto produto = new Produto();
 	private List<Produto> produtos;
-	
+
 	public Produto getProduto() {
 		return produto;
 	}
@@ -20,36 +19,44 @@ public class ProdutoBean {
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
-	
-	public void grava(){
+
+	public void grava() {
 		ProdutoDao dao = new ProdutoDao();
-		dao.adiciona(produto);
-		this.produto = new Produto();
+		if (produto.getId() == null){
+			dao.adiciona(produto);} else{
+				dao.atualiza(produto);
+			}
+		this.produto = new Produto(); //zera o valor do produto para poder cadastrar um novo
 		produtos = dao.listaTodos();
 	}
-	
+
 	public List<Produto> getProdutos() {
-		if(produtos == null){
+		if (produtos == null) {
 			System.out.println("Carregando produtos...");
 			produtos = new ProdutoDao().listaTodos();
 		}
 		return produtos;
 	}
-	
-	public Double getListaSoma(){
+
+	public Double getListaSoma() {
 		Double total = 0.0;
-		for(Produto produto:produtos){
+		for (Produto produto : produtos) {
 			total += produto.getPreco();
 		}
 		return total;
 	}
-	
-	public void remove(Produto produto){
+
+	public void remove(Produto produto) {
 		ProdutoDao dao = new ProdutoDao();
 		dao.remove(produto);
 		this.produtos = dao.listaTodos();
 	}
 	
+	public void cancelar(){
+		this.produto.setNome("");
+		this.produto.setDescricao("");
+		this.produto.setPreco(0.0);
+	}
 	
-	
+
 }
